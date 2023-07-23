461fc576a3db9c76e9eb7f46e136dfcf601050b5
haram22 <kimhr9889@handong.ac.kr>
2023-07-23 17:47:51 +0900

    Initial Commit

---
+++ Navi_code/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Navi_code
+//
+//  Created by 김하람 on 2023/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        makeUI();
+    }
+
+    private lazy var nextButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .black
+        button.tintColor = .white
+        button.setTitle("Login", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    func makeUI(){
+        view.addSubview(nextButton)
+        NSLayoutConstraint.activate([
+            nextButton.widthAnchor.constraint(equalToConstant: 140),
+            nextButton.heightAnchor.constraint(equalToConstant: 40),
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func buttonPressed(){
+        nextButton.backgroundColor = .magenta
+    }
+}
+
